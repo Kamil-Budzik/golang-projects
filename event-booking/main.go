de7b13858652510a09eb826ffa912bff9dfaace4
Kamil Budzik <mixboksu@gmail.com>
2024-12-19 13:33:43 +0100

    [EVENT-BOOKING] Create GET/POST handlers with DB connection

---
+++ event-booking/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kamil-budzik/event-booking/db"
+	"github.com/kamil-budzik/event-booking/models"
+)
+
+func main() {
+	db.InitDB()
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080")
+}
+
+func getEvents(ctx *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not fetch events. Try again later!",
+		})
+	}
+	ctx.JSON(http.StatusOK, events)
+}
+
+func createEvent(ctx *gin.Context) {
+	var event models.Event
+	err := ctx.ShouldBindJSON(&event)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "Could not parse request data.",
+		})
+		return
+	}
+
+	event.UserId = 1
+
+	err = event.Save()
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not create event. Try again later!",
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"message": "Event created!",
+		"event":   event,
+	})
+}
