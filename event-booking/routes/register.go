cc92740751a779392a2397bb7b14a45ab7bb5b41
Kamil Budzik <mixboksu@gmail.com>
2024-12-23 19:06:10 +0100

    [EVENT-BOOKING] add cancel registration handler

--- event-booking/routes/register.go
+++ event-booking/routes/register.go
@@ -34,4 +34,24 @@ func registerForEvent(ctx *gin.Context) {
 
 }
 
-func cancelRegistration(ctx *gin.Context) {}
+func cancelRegistration(ctx *gin.Context) {
+	userId := ctx.GetInt64("userId")
+	eventId := helpers.GetEventId(ctx)
+
+	var event models.Event
+	event.ID = eventId
+
+	err := event.CancelRegistration(userId)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not cancel registration",
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Registration cancelled",
+	})
+
+}
