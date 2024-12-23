e3c283da11d60d90060a694413a0b718dceeff2b
Kamil Budzik <mixboksu@gmail.com>
2024-12-23 19:01:20 +0100

    [EVENT-BOOKING] Register handler

---
+++ event-booking/routes/register.go
@@ -0,0 +1,37 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kamil-budzik/event-booking/helpers"
+	"github.com/kamil-budzik/event-booking/models"
+)
+
+func registerForEvent(ctx *gin.Context) {
+	userId := ctx.GetInt64("userId")
+	eventId := helpers.GetEventId(ctx)
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not fetch event",
+		})
+		return
+	}
+
+	err = event.Register(userId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not register user for event.",
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"message": "Registered",
+	})
+
+}
+
+func cancelRegistration(ctx *gin.Context) {}
