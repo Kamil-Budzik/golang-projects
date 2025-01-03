f56e8d5069a1aba03ddb7d0caaea77e0ee07903c
Kamil Budzik <mixboksu@gmail.com>
2025-01-03 12:15:04 +0100

    [SUDOKU-SOLVER] Make call from svelte to backend

--- sudoku-solver/frontend/src/lib/component/Sudoku.svelte
+++ sudoku-solver/frontend/src/lib/component/Sudoku.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
 	export let board: number[][] = Array(9).fill(Array(9).fill(0));
-	export let onInput: (updatedBoard: number[][]) => void = () => {};
 
 	function handleInput(event: Event, row: number, col: number): void {
 		const target = event.target as HTMLInputElement;
 		const value = parseInt(target.value) || 0;
 
 		if (value >= 0 && value <= 9) {
-			const updatedBoard = board.map((r, i) =>
-				i === row ? r.map((cell, j) => (j === col ? value : cell)) : r
-			);
-			onInput(updatedBoard);
+			board[row][col] = value;
 		}
 	}
 </script>
@@ -22,14 +18,8 @@
 				type="text"
 				maxlength="1"
 				value={cell === 0 ? '' : cell}
-				on:input={(e) => handleInput(e, rowIndex, colIndex)}
-				class={`w-10 h-10 text-center text-lg font-bold border
-					${(Math.floor(rowIndex / 3) + Math.floor(colIndex / 3)) % 2 === 0 ? 'bg-gray-100' : 'bg-white'}
-					${colIndex % 3 === 0 ? 'border-l-2' : ''}
-					${rowIndex % 3 === 0 ? 'border-t-2' : ''}
-					${colIndex === 8 ? 'border-r-2' : ''}
-					${rowIndex === 8 ? 'border-b-2' : ''}
-					border-gray-500`}
+				oninput={(e) => handleInput(e, rowIndex, colIndex)}
+				class={`h-10 w-10 border text-center text-lg font-bold ${(Math.floor(rowIndex / 3) + Math.floor(colIndex / 3)) % 2 === 0 ? 'bg-gray-100' : 'bg-white'} ${colIndex % 3 === 0 ? 'border-l-2' : ''} ${rowIndex % 3 === 0 ? 'border-t-2' : ''} ${colIndex === 8 ? 'border-r-2' : ''} ${rowIndex === 8 ? 'border-b-2' : ''} border-gray-500`}
 			/>
 		{/each}
 	{/each}
--- sudoku-solver/frontend/src/routes/+page.svelte
+++ sudoku-solver/frontend/src/routes/+page.svelte
@@ -1,9 +1,31 @@
 <script lang="ts">
 	import Sudoku from '$lib/component/Sudoku.svelte';
+	import axios from 'axios';
 
-	console.log('Hello from script');
+	let board: number[][] = Array(9).fill(null).map(() => Array(9).fill(0));
+
+	async function solveSudoku() {
+		try {
+			const apiURL = import.meta.env.VITE_API_BASE_URL;
+			const res = await axios.post(apiURL + '/solve', { board });
+
+			console.log(res.data);
+
+			// const solvedBoard = await response.json();
+			// board = solvedBoard.board; // Assuming the API returns the solved board in this format
+			// console.log('Solved board:', solvedBoard);
+		} catch (error) {
+			console.error('Failed to solve Sudoku:', error);
+		}
+	}
 </script>
 
 <main class="grid min-h-screen w-full place-items-center">
-	<Sudoku />
+	<Sudoku bind:board />
+	<button
+		on:click={solveSudoku}
+		class="mt-4 rounded bg-blue-500 px-4 py-2 text-white shadow-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
+	>
+		Solve
+	</button>
 </main>
